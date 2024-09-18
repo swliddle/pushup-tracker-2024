0d5fa0fe98ce3e940dc93a1676499bb8929dbd26
Steve Liddle <liddle@byu.edu>
2024-09-18 14:36:15 -0600

    Initial Commit

---
+++ Pushup Tracker 2024/Pushup_Tracker_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Pushup_Tracker_2024App.swift
+//  Pushup Tracker 2024
+//
+//  Created by Stephen Liddle on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Pushup_Tracker_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
